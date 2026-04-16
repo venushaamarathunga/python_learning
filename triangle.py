8c98f7d05c35c40e433f6f5bff19505a81f14fe1
venushaamarathunga <avenusha95@gmail.com>
2026-04-16 23:13:09 +0300

    update triangle

--- triangle.py
+++ triangle.py
@@ -4,4 +4,11 @@ def is_a_triangle(a, b, c):
     else:
         return False
 
-print(is_a_triangle(3, 4, 5))
+a = float(input('Enter the first side\'s length: '))
+b = float(input('Enter the second side\'s length: '))
+c = float(input('Enter the third side\'s length: '))
+
+if is_a_triangle(a, b, c):
+    print('Yes, it can be a triangle.')
+else:
+    print('No, it can\'t be a triangle.')
