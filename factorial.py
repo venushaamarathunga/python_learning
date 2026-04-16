e669b141ecc8a7707cbbdc879ae1f1afc970390e
venushaamarathunga <avenusha95@gmail.com>
2026-04-16 23:47:23 +0300

    update without recursion in factorial,fibonacci

--- factorial.py
+++ factorial.py
@@ -3,8 +3,11 @@ def factorial(n):
         return None
     elif n == 0 or n == 1:
         return 1
-    else:
-        return n * factorial(n - 1)
+    
+    product = 1
+    for i in range(2, n+1):
+        product *= i
+    return product
 
 for n in range(1, 6):
     print(n, " --> ", factorial(n))
--- fibonacci.py
+++ fibonacci.py
@@ -3,8 +3,13 @@ def fibonacci(n):
         return None
     elif n < 3:
         return 1
-    else:
-        return fibonacci(n - 1) + fibonacci(n -2)
+    
+    elem1 = elem2 = 1
+    sum = 0
+    for i in range(3 , n+1):
+        sum = elem1 + elem2 
+        elem1, elem2 = elem2, sum
+    return sum
     
 for n in range(1, 11):
     print(n, " --> ", fibonacci(n))
